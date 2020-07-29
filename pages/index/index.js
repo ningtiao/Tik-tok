5bb54fad7d621163e9a9ba7f284a11929ca43914
杨家馨 <578755236@qq.com>
2020-07-29 17:13:56 +0800

    去除无用代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -43,14 +43,6 @@ Page({
         rewardNum: '6'
       },
       {
-        videoUrl: "https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200f2f0000bg2dbhb6j2qj3mr8pa9g&line=0",
-        durations: 10,
-        poster: "https://p1.pstatp.com/large/12bea0008f8a226fc53c3.jpg",
-        likenum: 10,
-        commnetnum: '20',
-        rewardNum: '6'
-      },
-      {
         videoUrl: "https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200fce0000bg36q72j2boojh1t030g&line=0",
         durations: 10,
         poster: "https://p99.pstatp.com/large/12c5c0009891b32e947b7.jpg",
@@ -97,28 +89,11 @@ Page({
    */
   onLoad: function () {
     // 滑动
-    if (app.globalData.token) {
-      this.getVideos()
-      this.getUser()
-      this.getOwnInfo()
-    } else {
-      app.loginReadyCallback = res => {
-        this.getVideos()
-        this.getUser()
-        this.getOwnInfo()
-      }
-    }
     this.videoChange = throttle(this.touchEndHandler, 200)
     console.log(this.videoChange, 'this.videoChangethis.videoChange')
     // 绑定updateVideoIndex事件，更新当前播放视频index
     event.on('updateVideoIndex', this, function (index) {
       console.log('event updateVideoIndex:', index)
-      if (index == this.data.videos.length - 1) { //当滑动到最后一条加载下一条数据
-        this.setData({
-          nowPage: parseInt(this.data.nowPage + 1)
-        })
-        this.getVideos()
-      }
       setTimeout(() => {
         this.setData({
           animationShow: false,
@@ -137,63 +112,8 @@ Page({
    */
   onShow: function () {
     var that = this
-    wx.getStorage({
-      key: 'tag',
-      success: function (res) {
-        console.log(res, '555555')
-        that.setData({
-          tagIdList: res.data
-        })
-        console.log(that.data.tagIdList)
-      }
-    })
-    // const windowHeight = wx.getSystemInfoSync().windowHeight
     that.setData({
       windowHeight: wx.getSystemInfoSync().windowHeight
-    })
-  },
-  /**
-   * 获取视频数据
-   */
-  getVideos: function () {  // 获取视频列表
-    var that = this;
-    const params = {
-      accessToken: app.globalData.token,
-      nowPage: that.data.nowPage,
-      pageSize: '9'
-    }
-    Http.HttpRequst(false, '/api/lecture/getVideos', false, '', params, 'get', false, function (res) {
-      // that.setData({
-      //   videos: res.dataObject.list
-      // })
-      console.log(res.code == 102, '66')
-      if (res.code == 102) {
-        if (res.dataObject.list.length < that.data.rows) {
-          console.log(that.data.listData, '55')
-          that.setData({
-            videos: that.data.videos.concat(res.dataObject.list),
-          })
-        } else {
-          that.setData({
-            videos: that.data.videos.concat(res.dataObject.list),
-          })
-          console.log(that.data.videos)
-        }
-      } else if (res.code == 1001) {
-
-      }
-    })
-  },
-  bindplay() {
-    console.log('--- video play ---')
-  },
-  binderror(err) {
-    console.log(err)
-  },
-  bindtimeupdate(e) {
-    let percent = (e.detail.currentTime / e.detail.duration) * 100
-    this.setData({
-      percent: percent.toFixed(2)
     })
   },
   onReady: function () {
@@ -271,8 +191,6 @@ Page({
         animationShow: true
       }, () => {
         console.log('+1 切换')
-        console.log(index)
-        this.data.commentList = [] //滑动下一个视频清除评论列表
         this.createAnimation(1, index).then((res) => {
           this.setData({
             animation: this.animation.export(),
@@ -343,25 +261,6 @@ Page({
       animationShow: false,
     })
     this.vvideo.play()
-  },
-  onScrollLoad: function () {
-    // 加载新的数据
-    // this.getCommentList();
-    // wx.hideNavigationBarLoading();
-    console.log('触底了');
-    var that = this
-    if (that.data.loaderMore) {
-      that.setData({
-        hasmoreData: false,
-        hiddenloading: true,
-      })
-      setTimeout(function () {
-        that.setData({
-          page: parseInt(that.data.nowPage + 1)
-        })
-        that.getCommentList()
-      }, 500)
-    }
   },
   /// 双击
   doubleTap: function (e) {
@@ -489,83 +388,20 @@ Page({
    */
   addComment: function () {
 
-    var that = this
-    if (that.data.inputValue == '') {
-      // wx.showToast({
-      //   title: '内容不能为空',
-      //   icon: 'success',
-      //   duration: 2000
-      // })
-      return false
-    }
-    var params = {
-      contentId: that.data.contentId,
-      cont: that.data.inputValue
-    }
-    console.log(1)
-    Http.HttpRequst(false, '/api/lecture/addComment?accessToken=' + app.globalData.token, false, '', params, 'POST', false, function (res) {
-      if (res.code == 102) {
-        // that.setData({
-        //   commentList: res.
-        // })
-        that.setData({
-          inputValue: '',
-          commentList: []
-        })
-        const commnetnum = "videos[" + that.data.videoIndex + "].commnetnum"
-        console.log(that.data.commnetNum)
-        that.setData({
-          [commnetnum]: parseInt(that.data.commnetNum) + parseInt(1)
-        })
-        that.getCommentList()
-        console.log('评论成功')
-      } else if (res.code == 1001) {
-
-      }
-    })
   },
   /**
    * 点击头像关注
    */
   addLecturerFans: function (e) {
-    const that = this
-    const lecrId = e.currentTarget.dataset.lecturerid
-    const index = e.currentTarget.dataset.index
-    const isfans = "videos[" + index + "].isfans"
-    Http.HttpRequst(false, '/api/lecture/addLecturerFans?accessToken=' + app.globalData.token + '&lecrId=' + lecrId, false, '', '', 'POST', false, function (res) {
-      if (res.code == 102) {
-        that.setData({
-          [isfans]: 1
-        })
-      } else if (res.code == 1001) {
-
-      }
-    })
   },
   //粉丝取消关注
   delLecturerFans: function (e) {
-    const that = this
-    console.log(e)
-    const lecrId = e.currentTarget.dataset.lecturerid
-    const index = e.currentTarget.dataset.index
-    const isfans = "videos[" + index + "].isfans"
-    Http.HttpRequst(false, '/api/lecture/delLecturerFans?accessToken=' + app.globalData.token + '&lecrId=' + lecrId, false, '', '', 'POST', false, function (res) {
-      if (res.code == 102) {
-        that.setData({
-          [isfans]: 0
-        })
-      } else if (res.code == 1001) {
-
-      }
-    })
   },
   /**
    * 悬赏弹框组件
    */
   onShowModal: function (e) {
     // 显示弹框
-    console.log(e)
-    console.log(111111)
     this.setData({
       addingText: true,
       conid: e.currentTarget.dataset.conid,
@@ -579,91 +415,6 @@ Page({
     console.log(55566)
     this.setData({
       addingText: false
-    })
-  },
-  getUser: function () {
-    var that = this
-    wx.request({
-      url: app.globalData.baseUrl + '/api/game/getUser?accessToken=' + app.globalData.token,
-      data: {
-
-      },
-      header: {
-        "Content-Type": "applciation/json" //默认值
-      },
-      success: function (res) {
-        console.log(res)
-        that.setData({
-          gold: res.data.gold  //datas传值给页面的，可以自定义命名
-        })
-      },
-      fail: function (err) { },//请求失败
-      complete: function () { }//请求完成后执行的函数
-    })
-  },
-  onInputConfirm: function (e) { //赠送金币
-    // 隐藏弹框
-    console.log(e)
-    // this.toast.showToast('请选择要打赏的金币数目');
-    const that = this
-    const params = {
-      accessToken: app.globalData.token,
-      count: e.detail,
-      conid: that.data.conid,
-      lecid: that.data.lecid
-    }
-
-    // if (that.data.gold < 5) {
-    //   that.toast.showToast('金币不足');
-    //   return false
-    // }
-
-    // if (e.detail == '') {
-    //   that.toast.showToast('请选择要打赏的金币数目');
-    //   return false
-    // }
-    const rewardNum = "videos[" + that.data.indexVideo + "].rewardNum"
-    // wx.showModal({
-    //   title: '提示',
-    //   content: '确定要打赏' + e.detail + '金币吗?',
-    //   confirmText: "确认",
-    //   cancelText: "取消",
-    //   success: function (res) {
-    //     console.log(res);
-    //     if (res.confirm) {
-    //       Http.HttpRequst(false, '/api/lecture/reward?accessToken=' + params.accessToken + '&conid=' + params.conid + '&lecid=' + params.lecid + '&count=' + params.count, false, '', '', 'POST', false, function (res) {
-    //         console.log(res.code == 102, '66')
-    //         if (res.code == 102) {
-    //           that.setData({
-    //             [rewardNum]: parseInt(1) + parseInt(that.data.rewardNum),
-    //             addingText: false
-    //           })
-    //           wx.showToast({
-    //             title: '打赏成功！',
-    //             icon: 'success',
-    //             duration: 2000
-    //           });
-    //           // that.countdown = that.selectComponent('#modalShow');
-    //           // that.countdown.hideInputBox();
-    //           that.getUser()
-    //         } else if (res.code == 101) {
-
-    //         } else {
-
-    //         }
-    //       })
-    //     } else {
-    //       console.log('用户点击辅助操作')
-    //     }
-    //   }
-    // });
-  },
-  goAddVideo: function () {
-    wx.removeStorage({
-      key: 'tag'
-    })
-    wx.navigateTo({
-      url: '/pages/uploader/uploader'
     })
   }
 })
